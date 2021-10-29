b7ec9159ec323590e40d0825966983d80f60342b
siraajsk <siraajsk4@gmail.com>
2021-10-29 13:05:00 -0700

    Fixed tests on main

--- tests/test.cpp
+++ tests/test.cpp
@@ -22,37 +22,37 @@ TEST(ConstructorTest, ZeroParams) {
 
 TEST(AreaTest, TwoParams) {
 	Rectangle *r = new Rectangle(4, 4);
-	EXPECT_EQ(r->area(), 8);
+	EXPECT_EQ(r->area(), 16);
 	delete r;
 }
 
 TEST(AreaTest, TryingZero) {
 	Rectangle *r = new Rectangle(0, 0);
-	EXPECT_EQ(r->area(), 5);
+	EXPECT_EQ(r->area(), 0);
 	delete r;
 }
 
-TEST(AreaTest, AreaIsZero) {
-	Rectangle *r = new Rectangle();
-	EXPECT_EQ(r->area(), 0);
+TEST(AreaTest, NegativeParam) {
+	Rectangle *r = new Rectangle(-1, 2);
+	EXPECT_EQ(r->area(), -2);
 	delete r;
 }
 
 TEST(PerimeterTest, TwoParams) {
 	Rectangle *r = new Rectangle(1,2);
-	EXPECT_EQ(r->perimeter(), 5);
+	EXPECT_EQ(r->perimeter(), 6);
 	delete r;
 }
 
 TEST(PerimeterTest, UsingZero) {
-	Rectangle *r = new Rectangle();
+	Rectangle *r = new Rectangle(0, 0);
 	EXPECT_EQ(r->perimeter(), 0);
 	delete r;
 }
 
-TEST(PerimeterTest, ZeroInConstructor) {
-	Rectangle *r = new Rectangle(0,1);
-	EXPECT_EQ(r->perimeter(), 10);
+TEST(PerimeterTest, NegativeParam) {
+	Rectangle *r = new Rectangle(-1,2);
+	EXPECT_EQ(r->perimeter(), 2);
 	delete r;
 }
 
