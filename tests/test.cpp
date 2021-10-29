f3e15eb86dd1d12d1380c812f84e2a910d2d53e5
siraajsk <siraajsk4@gmail.com>
2021-10-29 07:47:31 -0700

    Added failing tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -13,7 +13,21 @@ TEST(ConstructorTest, TwoParams) {
 	delete r;
 }
 
+TEST(FailingConstructorTest, NoParams) {
+        Rectangle* r = new Rectangle();
+        EXPECT_EQ(r->area(), 1);
+        delete r;
+}
+
+TEST(FailingConstructorTest, TwoParams) {
+        Rectangle* r = new Rectangle(1, 2);
+        EXPECT_EQ(r->area(), 0);
+        delete r;
+}
+
 int main(int argc, char **argv) {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }
+
+
